b259259bf1ddcbe88a6290e090ca6eb0516117d6
AceIsHuman <menoman1995@yahoo.com>
2019-12-04 21:48:51 -0600

    connect userRouter

--- api/api-router.js
+++ api/api-router.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+const usersRouter = require('./users/usersRoute.js');
+
+router.use('/users', usersRouter);
+
 router.get('/', (req, res) => {
     res.json({ message: "It's alive!" });
 });
