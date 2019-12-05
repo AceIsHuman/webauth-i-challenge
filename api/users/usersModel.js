1a9c3d46b30bb23b382d47c25a67bd9b3309f747
AceIsHuman <menoman1995@yahoo.com>
2019-12-04 21:19:21 -0600

    add findById helper function

--- api/users/usersModel.js
+++ api/users/usersModel.js
@@ -9,6 +9,10 @@ function find() {
     return db('users');
 };
 
+function findById(id) {
+    return db('users').where({ id });
+}
+
 function insert(user) {
     return db('users').insert(user).then(([id]) => findById(id));
 }
