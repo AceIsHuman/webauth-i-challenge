9717dfa34f69957c9fe34e0140c659a1b739fc81
AceIsHuman <menoman1995@yahoo.com>
2019-12-04 20:51:28 -0600

    create api-router

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
+const apiRouter = require('./api-router.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.use('/api', apiRouter);
 
 module.exports = server;
