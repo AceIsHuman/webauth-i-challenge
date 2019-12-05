bf403d7dff555fbb1c713e93c3327f50ee41123a
AceIsHuman <menoman1995@yahoo.com>
2019-12-05 02:49:26 -0600

    add get route using auth middleware

--- api/users/usersRoute.js
+++ api/users/usersRoute.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const Users = require('./usersModel.js');
 const bcrypt = require('bcryptjs');
 
+const authRequired = require('../auth/auth-required.js');
+
 router.post('/register', async (req, res) => {
     const user = req.body;
 
@@ -14,6 +16,11 @@ router.post('/register', async (req, res) => {
     } catch(error) {
         res.status(500).json({ message: "Unable to create user.", error });
     }
+});
+
+router.get('/', authRequired, async (req, res) => {
+        const users = await Users.find();
+        res.status(200).json(users);
 });
 
 module.exports = router;
