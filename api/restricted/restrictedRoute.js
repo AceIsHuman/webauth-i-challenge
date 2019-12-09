cc5b67994a1fb3d83393b8188615fce275920ee0
AceIsHuman <menoman1995@yahoo.com>
2019-12-08 22:22:26 -0600

    add more endpoints

--- api/restricted/restrictedRoute.js
+++ api/restricted/restrictedRoute.js
@@ -7,4 +7,16 @@ router.get('/', (req, res) => {
   res.json({ message: "I see you've discovered the restricted path." });
 })
 
+router.get('/something', (req, res) => {
+  res.json({ message: "I see you've discovered the restricted path." });
+})
+
+router.get('/other', (req, res) => {
+  res.json({ message: "I see you've discovered the restricted path." });
+})
+
+router.get('/a', (req, res) => {
+  res.json({ message: "I see you've discovered the restricted path." });
+})
+
 module.exports = router;
