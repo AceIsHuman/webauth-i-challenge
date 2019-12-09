9da59c485645c6babf58025e46ff713d82ba034a
AceIsHuman <menoman1995@yahoo.com>
2019-12-08 19:45:05 -0600

    add express-session and create sessionOptions

--- api/server.js
+++ api/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const helmet = require('helmet');
 const apiRouter = require('./api-router.js');
+const session = require('express-session');
 
 const server = express();
 
+const sessionOptions = {
+  name: process.env.SESSION_NAME || 'mycookie',
+  secret: process.env.SESSION_PASS || 'cookiesareyummyyummy',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false
+}
+
 server.use(helmet());
 server.use(express.json());
+server.use(session(sessionOptions))
 
 server.use('/api', apiRouter);
 
