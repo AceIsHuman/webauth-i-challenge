0afeba8699be7b0630f888defca9d59783129173
AceIsHuman <menoman1995@yahoo.com>
2019-12-08 20:49:30 -0600

    refactor to use session to authenticate user

--- api/auth/auth-required.js
+++ api/auth/auth-required.js
@@ -2,18 +2,10 @@ const Users = require('../users/usersModel.js');
 const bcrypt = require('bcryptjs');
 
 async function authReq(req, res, next) {
-    const { user, password } = req.headers;
-
-    if(!(user && password)) {
-        res.status(401).json({ message: "Provide both USER and PASSWORD." });
+    if(!(req.session && req.session.userId)) {
+        res.status(400).json({ message: "No credentials provided." });
     } else {
-        try {
-            const _user = await Users.findBy({ user }).first();
-            if(_user && bcrypt.compareSync(password, _user.password)) next();
-            else res.status(401).json({ message: "You shall not pass!"});
-        } catch(error) {
-            res.status(500).json(error);
-        }
+        next();
     }
 }
 
